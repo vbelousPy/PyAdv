5ecd5a2a62e906173e63fcfab5bb0cc1511d5a96
Admin <v.belous@outlook.com>
2018-10-05 21:58:26 +0300

    main_2

--- lesson_1/main_2.py
+++ lesson_1/main_2.py
@@ -4,8 +4,7 @@ import random
 def foo(x):
     if 0 < x < 1:
         my_dict = dict({"before": [], "after": []})
-        my_list = [random.random() for i in range(50)]
-        for i in my_list:
+        for i in [random.random() for i in range(50)]:
             if i < x:
                 my_dict.get("before").append(i)
             else:
