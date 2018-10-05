8db954f6bf689517d864b2be7d0c996aa31f5d11
Admin <v.belous@outlook.com>
2018-10-05 21:37:53 +0300

    main_1

--- lesson_1/main_1.py
+++ lesson_1/main_1.py
@@ -1,3 +1,13 @@
-print("three")
-print("four")
-print("five")
+def foo(s):
+    return (ord(i) for i in s)
+
+
+def test_script():
+    result = foo("xYz")
+    if list(result) == [120, 89, 122]:
+        print("Ok")
+    else:
+        print("Error")
+
+
+test_script()
