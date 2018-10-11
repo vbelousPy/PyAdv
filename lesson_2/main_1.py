c01ba5d4a32810c2f7a3581d1d1cac730f3dd25c
vbelous <vbbelous@gmail.com>
2018-10-11 13:11:01 +0300

    classwork_1.fixed

--- lesson_2/main_1.py
+++ lesson_2/main_1.py
@@ -2,11 +2,11 @@ def unpacker(*args):
     my_list = [i for i in args]
     for i in range(len(my_list)):
         if isinstance(my_list[i], list):
-            temp_object = my_list[i]
-            my_list.remove(temp_object)
-            my_list.extend(temp_object)
-            my_list = unpacker(*my_list)
+            temp_object = my_list[0:i]
+            temp_object.extend(my_list[i])
+            temp_object.extend(my_list[i + 1:])
+            my_list = unpacker(*temp_object)
     return my_list
 
 
-print(unpacker(1, 2, 3, [1, 2], [3, 4, [5], [6]]))
+print(unpacker(1, 2, 3, [1, 2], [3, 4, [5, [66]], [6]]))
