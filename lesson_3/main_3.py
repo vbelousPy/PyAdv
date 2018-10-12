3acba490c5e055ac84883e4354bcee8affc307fe
Admin <v.belous@outlook.com>
2018-10-12 22:01:40 +0300

    lesson_3

---
+++ lesson_3/main_3.py
@@ -0,0 +1,19 @@
+import csv
+
+
+def csv_reader(file_name):
+    my_dict = dict()
+    with open(file_name, "r") as f:
+        reader = csv.reader(f)
+        title = 1
+        for row in reader:
+            for i in range(len(row)):
+                current_number = row[i]
+                if my_dict.get("#" + str(title)) is None or current_number > my_dict.get("#" + str(title)):
+                    my_dict.update({"#" + str(title):current_number})
+            title +=1
+    return my_dict
+
+def json_writer
+# csv_reader("my.csv")
+print(csv_reader("my.csv"))
